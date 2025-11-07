4e03094c575fc15221d5a98d2af6b27e49adb4fe
meehai <mihaicristianpirvu@gmail.com>
2025-11-07 18:53:04 +0200

    did u?

--- drone_ioact/olympe/__init__.py
+++ drone_ioact/olympe/__init__.py
@@ -1,9 +1,13 @@
 """init file"""
-import olympe
+try:
+    import olympe
 
-from .olympe_actions_maker import OlympeActionsMaker
-from .olympe_frame_reader import OlympeFrameReader
+    from .olympe_actions_maker import OlympeActionsMaker
+    from .olympe_frame_reader import OlympeFrameReader
 
-olympe.log.update_config({"loggers": {"olympe": {"level": "CRITICAL"}}})
+    olympe.log.update_config({"loggers": {"olympe": {"level": "CRITICAL"}}})
 
-__all__ = ["OlympeActionsMaker", "OlympeFrameReader"]
+    __all__ = ["OlympeActionsMaker", "OlympeFrameReader"]
+except ImportError as e:
+    from drone_ioact.utils import logger
+    logger.warning(f"olympe could not be imported {e}. Did you run 'pip install -r requirements-drones.txt' ?")
