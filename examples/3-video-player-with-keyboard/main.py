d7b0e9f2049b48679d10cb11ce1b981b3796a89c
meehai <mihaicristianpirvu@gmail.com>
2025-11-08 08:22:36 +0200

    split main and video container in two for a bit of modularity

--- examples/3-video-player-with-keyboard/main.py
+++ examples/3-video-player-with-keyboard/main.py
@@ -13,45 +13,14 @@ from drone_ioact import DroneIn, Action, ActionsQueue, ActionsProducer
 from drone_ioact.data_consumers import ScreenDisplayer, KeyboardController
 from drone_ioact.utils import logger
 
+from video_container import VideoContainer
+
 QUEUE_MAX_SIZE = 30
 
 class MyActionsQueue(ActionsQueue):
     """Defines the actions of this video player"""
     def get_actions(self) -> list[Action]:
         return ["DISCONNECT", "PLAY_PAUSE", "SKIP_AHEAD_ONE_SECOND", "GO_BACK_ONE_SECOND"]
-
-class VideoContainer(threading.Thread):
-    """This 'acts' as a drone and the only action we can control is the frame number or if it's paused"""
-    def __init__(self, video: VREVideo):
-        super().__init__()
-        self.video = video
-        self.frame_ix = 0
-        self.is_paused = True
-        self.is_done = False
-        self.fps = video.fps
-        self._current_frame: np.ndarray | None = None
-        self._current_frame_lock = threading.Lock()
-
-    def get_current_frame(self) -> np.ndarray:
-        """gets the current frame in a thread safe way"""
-        with self._current_frame_lock:
-            return self._current_frame
-
-    def increment_frame(self, n: int):
-        """thread-safe way to increment the frame. Called by the actions maker on key presses"""
-        with self._current_frame_lock:
-            self.frame_ix = (self.frame_ix + int(n)) % len(self.video)
-
-    def run(self):
-        self.is_paused = False
-        while not self.is_done:
-            now = datetime.now()
-            with self._current_frame_lock:
-                self._current_frame = self.video[self.frame_ix]
-            if not self.is_paused:
-                self.increment_frame(n=1)
-            if (diff := (1 / self.fps - (datetime.now() - now).total_seconds())) > 0:
-                time.sleep(diff)
 
 class VideoFrameReader(DroneIn):
     """VideoFrameReader gets data from a video container (producing frames in real time)"""
@@ -89,10 +58,8 @@ class VideoActionsMaker(ActionsProducer, threading.Thread):
 
 def main():
     """main fn"""
-    video = VREVideo(sys.argv[1])
-    logger.info(f"Read video: {video}")
+    (video_container := VideoContainer(sys.argv[1])).start() # start the video thread so it produces "real time" frames
     actions_queue = MyActionsQueue(Queue(maxsize=QUEUE_MAX_SIZE))
-    (video_container := VideoContainer(video)).start() # start the video thread so it produces "real time" frames
 
     # data producer thread (1) (drone I/O in -> data/RGB out)
     video_frame_reader = VideoFrameReader(video=video_container)
