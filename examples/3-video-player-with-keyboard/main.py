b0985c06eddf99e2e7ce9df0ee24bc3ffedabb64
meehai <mihaicristianpirvu@gmail.com>
2025-11-08 07:46:21 +0200

    video is also a thread akin to a drone flying in real time produing frames at fps speed

--- examples/3-video-player-with-keyboard/main.py
+++ examples/3-video-player-with-keyboard/main.py
@@ -19,35 +19,54 @@ class MyActionsQueue(ActionsQueue):
     def get_actions(self) -> list[Action]:
         return ["DISCONNECT", "PLAY_PAUSE", "SKIP_AHEAD_ONE_SECOND", "GO_BACK_ONE_SECOND"]
 
-class VideoWithFrames(VREVideo):
-    """This 'acts' as a drone and the only action we can control is the frame number"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class VideoContainer(threading.Thread):
+    """This 'acts' as a drone and the only action we can control is the frame number or if it's paused"""
+    def __init__(self, video: VREVideo):
+        super().__init__()
+        self.video = video
         self.frame_ix = 0
+        self.is_paused = True
+        self.is_done = False
+        self.fps = video.fps
+        self._current_frame: np.ndarray | None = None
+        self._current_frame_lock = threading.Lock()
+
+    def get_current_frame(self) -> np.ndarray:
+        """gets the current frame in a thread safe way"""
+        with self._current_frame_lock:
+            return self._current_frame
+
+    def increment_frame(self, n: int):
+        """thread-safe way to increment the frame. Called by the actions maker on key presses"""
+        with self._current_frame_lock:
+            self.frame_ix = (self.frame_ix + int(n)) % len(self.video)
+
+    def run(self):
         self.is_paused = False
+        while not self.is_done:
+            with self._current_frame_lock:
+                self._current_frame = self.video[self.frame_ix]
+            if not self.is_paused:
+                self.increment_frame(n=1)
+            time.sleep(1 / self.video.fps)
 
 class VideoFrameReader(DroneIn):
     """VideoFrameReader gets data from a video file"""
-    def __init__(self, video: VideoWithFrames):
+    def __init__(self, video: VideoContainer):
         self.video = video
-        self._is_streaming = True
 
     def get_current_data(self, timeout_s: int = 10) -> dict[str, np.ndarray]:
-        data = self.video[self.video.frame_ix]
-        # TODO: kinda hacky that the data receiver appends this
-        if not self.video.is_paused:
-            self.video.frame_ix = (self.video.frame_ix + 1) % len(self.video)
-        return {"rgb": data}
+        return {"rgb": self.video.get_current_frame()}
 
     def is_streaming(self) -> bool:
-        return self._is_streaming
+        return not self.video.is_done
 
     def stop_streaming(self):
-        self._is_streaming = False
+        self.video.is_done = True
 
 class VideoActionsMaker(ActionsProducer, threading.Thread):
     """VideoActionsMaker defines the actions taken on the video container based on the actions produced"""
-    def __init__(self, video: VideoWithFrames, actions_queue: Queue):
+    def __init__(self, video: VideoContainer, actions_queue: Queue):
         ActionsProducer.__init__(self, actions_queue)
         threading.Thread.__init__(self)
         self.video = video
@@ -60,18 +79,19 @@ class VideoActionsMaker(ActionsProducer, threading.Thread):
             if item == "PLAY_PAUSE":
                 self.video.is_paused = not self.video.is_paused
             if item == "SKIP_AHEAD_ONE_SECOND":
-                self.video.frame_ix = int(self.video.frame_ix + self.video.fps) % len(self.video)
+                self.video.increment_frame(self.video.fps)
             if item == "GO_BACK_ONE_SECOND":
-                self.video.frame_ix = int(self.video.frame_ix - self.video.fps) % len(self.video)
+                self.video.increment_frame(-self.video.fps)
 
 def main():
     """main fn"""
-    video = VideoWithFrames(sys.argv[1])
+    video = VREVideo(sys.argv[1])
     logger.info(f"Read video: {video}")
     actions_queue = MyActionsQueue(Queue(maxsize=QUEUE_MAX_SIZE))
+    (video_container := VideoContainer(video)).start()
 
     # data producer thread (1) (drone I/O in -> data/RGB out)
-    video_frame_reader = VideoFrameReader(video=video)
+    video_frame_reader = VideoFrameReader(video=video_container)
     # data consumer threads (data/RGB in -> I/O out)
     screen_displayer = ScreenDisplayer(drone_in=video_frame_reader)
     # data consumer & actions producer threads (data/RGB in -> action out)
@@ -80,7 +100,7 @@ def main():
     kb_controller = KeyboardController(drone_in=video_frame_reader, actions_queue=actions_queue,
                                        key_to_action=key_to_action)
     # actions consumer thread (1) (action in -> drone I/O out)
-    video_actions_maker = VideoActionsMaker(video=video, actions_queue=actions_queue)
+    video_actions_maker = VideoActionsMaker(video=video_container, actions_queue=actions_queue)
 
     threads: dict[str, threading.Thread] = {
         "Keyboard controller": kb_controller,
