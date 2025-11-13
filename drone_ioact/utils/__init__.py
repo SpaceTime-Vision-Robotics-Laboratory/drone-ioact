b99dd3e6e6b792c0f80525d1728f7bfe2875b584
meehai <mihaicristianpirvu@gmail.com>
2025-11-13 13:33:08 +0200

    split thread group from utils

---
+++ drone_ioact/utils/__init__.py
@@ -0,0 +1,5 @@
+"""init file for generic utils"""
+from .utils import logger
+from .thread_group import ThreadGroup
+
+__all__ = ["logger", "ThreadGroup"]
--- drone_ioact/utils/thread_group.py
+++ drone_ioact/utils/thread_group.py
@@ -1,14 +1,7 @@
-"""generic utils file"""
-from pathlib import Path
-from datetime import datetime
+"""thread_group.py"""
 import threading
-from loggez import make_logger
 
-def get_project_root() -> Path:
-    """returns the project root"""
-    return Path(__file__).parents[1]
-
-logger = make_logger("DRONE", log_file=Path.cwd() / f"{get_project_root()}/logs/{datetime.now().isoformat()[0:-6]}.txt")
+from .utils import logger
 
 class ThreadGroup(dict):
     """Thread group is a utility dictionary container (str->thread) for managing many threads at once"""
