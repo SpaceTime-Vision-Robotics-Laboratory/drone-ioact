d38c9586bb439e3245dcb7dd7d446f07385cb11c
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2025-11-21 10:47:16 +0200

    Rename and separated drone examples from video examples

--- drone_ioact/drones/video/__init__.py
+++ drone_ioact/drones/video/__init__.py
@@ -4,7 +4,9 @@ try:
     from .video_player import VideoPlayer
     from .video_data_producer import VideoDataProducer
     from .video_actions_consumer import VideoActionsConsumer
-    __all__ = ["VideoPlayer", "VideoDataProducer", "VideoActionsConsumer"]
+    from .video_actions import video_actions_callback, VIDEO_SUPPORTED_ACTIONS
+    __all__ = ["VideoPlayer", "VideoDataProducer", "VideoActionsConsumer",
+               "video_actions_callback", "VIDEO_SUPPORTED_ACTIONS"]
 except ImportError as e:
     from drone_ioact.utils import logger
     logger.warning(f"video container could not be imported {e}. Did you run 'pip install -r requirements-drones.txt' ?")
