89fe9e05d6d6329a9876aea09138be9762766f4f
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2026-01-18 16:58:18 +0200

    DataProducerList: topological sort based on provided Data Producers

--- robobase/utils/utils.py
+++ robobase/utils/utils.py
@@ -1,6 +1,7 @@
 """generic utils file"""
 from pathlib import Path
 from datetime import datetime
+from typing import Any
 from loggez import make_logger
 
 def get_project_root() -> Path:
@@ -10,3 +11,7 @@ def get_project_root() -> Path:
 logs_dir = get_project_root() / "logs"
 
 logger = make_logger("ROBOBASE", log_file=f"{logs_dir}/{datetime.now().isoformat()[0:-7]}/ROBOBASE.txt")
+
+def parsed_str_type(item: Any) -> str:
+    """Given an object with a type of the format: <class 'A.B.C.D'>, parse it and return 'A.B.C.D'"""
+    return str(type(item)).rsplit(".", maxsplit=1)[-1][0:-2]
